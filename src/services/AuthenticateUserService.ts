fc39dc40e8c942a94445a3fc51a5f4de3901a1f9
Denis Toledo <contato@denistoledo.com.br>
2020-10-01 10:18:27 -0300

    add errors handler

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,8 +1,10 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-import User from '../models/User';
+
 import authConfig from '../config/auth';
+import User from '../models/User';
+import AppError from '../errors/AppError';
 
 interface Request {
   email: string;
@@ -21,13 +23,13 @@ class AuthenticateUserService {
     const user = await usersRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorrect email/passsword combination.');
+      throw new AppError('Incorrect email/passsword combination.', 401);
     }
     
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorrect email/passsword combination.');
+      throw new AppError('Incorrect email/passsword combination.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
